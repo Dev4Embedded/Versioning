2d27f476907321765a0828594fc6ad119084287a
Dev4Embedded <integralzerox@gmail.com>
2019-08-07 21:58:24 +0200

    Information on compilation time & date

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,21 @@
 input="../inc/version.h"
-MAJOR=0;
-MINOR=0;
-PREV_NO_COMP=0;
+
+MAJOR=0
+MINOR=0
+PREV_NO_COMP=0
+
+#Get current date:
+YEARS=$(date +"%Y")
+MONTHS=$(date +"%m")
+DAYS=$(date +"%d")
+DATE="$DAYS.$MONTHS.$YEARS"
+
+#Get current time:
+HOURS=$(date +"%H")
+MINUTES=$(date +"%M")
+SECONDS=$(date +"%S")
+TIME="$HOURS:$MINUTES:$SECONDS"
+
 while read -r line
 do
 	if [[ $line  == *"#define VERSION_MAJOR"* ]]; then
@@ -12,13 +26,13 @@ do
     fi
     if [[ $line == *"#define VERSION_COMP_NUMBER"* ]]; then
     	PREV_NO_COMP=${line##*VERSION_COMP_NUMBER }
-    	echo PREV_NO_COMP;
     fi
 done < "$input"
 
-
 echo "Major version: $MAJOR"
 echo "Minor version: $MINOR"
+echo "Time: $TIME"
+echo "Date: $DATE"
 #echo "Previous number of compilation: $PREV_NO_COMP"
 
 CURR_NO_COMP=$((PREV_NO_COMP+1))
@@ -26,4 +40,11 @@ CURR_NO_COMP=$((PREV_NO_COMP+1))
 echo "Current number of compilation: $CURR_NO_COMP"
 
 sed -i "s/#define VERSION_COMP_NUMBER $PREV_NO_COMP/#define VERSION_COMP_NUMBER $CURR_NO_COMP/g" "$input"
+sed -i "s/#define VERSION_DATE_YEAR ..../#define VERSION_DATE_YEAR $YEARS/g" "$input"
+sed -i "s/#define VERSION_DATE_MONTH ../#define VERSION_DATE_MONTH $MONTHS/g" "$input"
+sed -i "s/#define VERSION_DATE_DAY ../#define VERSION_DATE_DAY $DAYS/g" "$input"
+sed -i "s/#define VERSION_TIME_HOUR ../#define VERSION_TIME_HOUR $HOURS/g" "$input"
+sed -i "s/#define VERSION_TIME_MINUTE ../#define VERSION_TIME_MINUTE $MINUTES/g" "$input"
+sed -i "s/#define VERSION_TIME_SECOND ../#define VERSION_TIME_SECOND $SECONDS/g" "$input"
+
 
